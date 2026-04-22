da8e5748b933b88d2d01d4e989d999679dc67a11
HenishPatel1214 <130112154+HenishPatel1214@users.noreply.github.com>
2026-04-21 21:41:22 -0600

    Add comments to signals in ModeSelectionPage

--- include/pages/ModeSelectionPage.h
+++ include/pages/ModeSelectionPage.h
@@ -13,11 +13,19 @@ class ModeSelectionPage : public QWidget
     Q_OBJECT
 
 public:
+    // Constructs the mode selection page and optionally assigns a parent widget.
     explicit ModeSelectionPage(QWidget *parent = nullptr);
 
 signals:
+    // Emitted when the user chooses Fan Mode.
     void fanModeSelected();
+
+    // Emitted when the user chooses Player Mode.
     void playerModeSelected();
+
+    // Emitted when the user chooses the Bracket option.
     void bracketSelected();
+
+    // Emitted when the user requests navigation to the previous/back page.
     void backRequested();
 };
