b1d3267f9cecdd49262865b94139f6f631bf8738
HenishPatel1214 <130112154+HenishPatel1214@users.noreply.github.com>
2026-04-21 23:29:49 -0600

    Clarify comments in PlayerModePage header
    Updated comments to clarify the purpose of PlayerModePage.

--- include/pages/PlayerModePage.h
+++ include/pages/PlayerModePage.h
@@ -4,7 +4,8 @@
 // Team: Ebrahim Himayoun, Tom Rasmussen, Diego Galavan,
 //       Henish Patel, Reman Adhikari, Ethan Cobler
 //
-// PlayerModePage = simple menu page for Player mode.
+// PlayerModePage displays the Player mode menu
+// and lets the user open lessons, go back, or start the game.
 
 #include <QWidget>
 
@@ -13,10 +14,16 @@ class PlayerModePage : public QWidget
     Q_OBJECT
 
 public:
+    // Creates the Player mode page.
     explicit PlayerModePage(QWidget *parent = nullptr);
 
 signals:
+    // Opens the Player mode lessons page.
     void openLessonsRequested();
+
+    // Returns to the previous page.
     void backRequested();
+
+    // Starts the Player mode game.
     void playGameClicked();
 };
